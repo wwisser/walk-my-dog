5940eac07b56b219c7318490634babc6c1060223
Wendelin Wisser <49454122+wwisser@users.noreply.github.com>
2020-02-07 11:25:33 +0100

    Add auth filter

--- api/src/main/java/de/walkmydog/api/user/AuthRoute.java
+++ api/src/main/java/de/walkmydog/api/user/AuthRoute.java
@@ -2,7 +2,10 @@ package de.walkmydog.api.user;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.eclipse.jetty.http.HttpStatus;
 import org.mindrot.jbcrypt.BCrypt;
 import spark.Request;
@@ -10,6 +13,11 @@ import spark.Response;
 import spark.Spark;
 
 public class AuthRoute {
+    
+    private static final Set<String> EXCLUDED_ROUTES = Stream.of(
+        "/auth",
+        "/register"
+    ).collect(Collectors.toSet());
 
     private final UserStorage userStorage;
     private final Gson gson = new Gson();
@@ -20,11 +28,30 @@ public class AuthRoute {
 
         Spark.post("/auth", this::handleAuthorization);
         Spark.post("/register", this::handleRegistration);
+        
+        Spark.before((request, response) -> {
+            if (EXCLUDED_ROUTES.contains(request.pathInfo())) {
+                return;
+            }
+            
+            String authHeader = request.headers("Authorization");
+            
+            if (authHeader == null || authHeader.trim().isEmpty()) {
+                Spark.halt(HttpStatus.UNAUTHORIZED_401, "Authorization Header not given.");
+                return;
+            }
+            
+            UUID token = UUID.fromString(authHeader);
+            boolean authorized = this.tokenRegistry.checkAndUpdate(token);
+            
+            if (!authorized) {
+                Spark.halt(HttpStatus.UNAUTHORIZED_401, "Authorization token invalid.");
+            }
+        });
     }
 
     private Object handleAuthorization(Request request, Response response) throws Exception {
         Credentials credentials = this.gson.fromJson(request.body(), Credentials.class);
-        
         User user = this.userStorage.findByName(credentials.name);
         
         if (null == user) {
@@ -53,6 +80,11 @@ public class AuthRoute {
     private Object handleRegistration(Request request, Response response) throws Exception {
         Credentials credentials = this.gson.fromJson(request.body(), Credentials.class);
 
+        if (credentials.name == null || credentials.password == null) {
+            Spark.halt(HttpStatus.BAD_REQUEST_400, "User credentials not given");
+            return null;
+        }
+        
         if (this.userStorage.findByName(credentials.name) != null) {
             Spark.halt(HttpStatus.CONFLICT_409, "User name already exists.");
         }
