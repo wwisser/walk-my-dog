00bec5d24f49cba01c1a3ad01df602e1f4b4cf4a
Wisser <wisserwendelin@gmail.com>
2020-02-21 23:00:10 +0100

    Integrate mongo database connection

--- api/src/main/java/de/walkmydog/api/WalkMyDog.java
+++ api/src/main/java/de/walkmydog/api/WalkMyDog.java
@@ -9,38 +9,46 @@ import org.bson.codecs.pojo.PojoCodecProvider;
 import org.bson.codecs.configuration.CodecRegistries;
 import spark.Spark;
 
+import java.util.function.Consumer;
+
 public class WalkMyDog {
 
-    private static final int PORT = 8080;
-    private static final String MONGO_HOST = "localhost";
+    private static final int SPARK_PORT = 8080;
+    private static final String MONGO_HOST = "78.47.142.22";
 
     public static void main(String... args) {
-        Spark.port(PORT);
+        Spark.port(SPARK_PORT);
         Spark.get("/ping", (request, response) -> {
             response.status(200);
             return "Health Check OK";
         });
-        
+
         new AuthRoute(new LocalUserStorage());
-        
-        CodecRegistry pojoCodecRegistry = CodecRegistries
-                .fromRegistries(
-                    MongoClient.getDefaultCodecRegistry(),
-                    CodecRegistries.fromProviders(
-                            PojoCodecProvider
-                                    .builder()
-                                    .automatic(true)
-                                    .build()
-                    )
-                );
-        
-        MongoClient mongoClient = new MongoClient(
-                MONGO_HOST,
-                MongoClientOptions
-                        .builder()
-                        .codecRegistry(pojoCodecRegistry)
-                        .build()
+
+        CodecRegistry pojoCodecRegistry = CodecRegistries.fromRegistries(
+            MongoClient.getDefaultCodecRegistry(),
+            CodecRegistries.fromProviders(
+                PojoCodecProvider
+                    .builder()
+                    .automatic(true)
+                    .build()
+            )
         );
+
+        MongoClient mongoClient = new MongoClient(
+            MONGO_HOST,
+            MongoClientOptions
+                .builder()
+                .codecRegistry(pojoCodecRegistry)
+                .build()
+        );
+
+        mongoClient.listDatabaseNames().forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println(s);
+            }
+        });
     }
 
 }
